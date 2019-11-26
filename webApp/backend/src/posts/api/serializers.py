a4caeb53b6bf8e63063503fdee525501a7fda7f7
Jeff Chang <jeff@Jeffs-MacBook-Pro.local>
2019-11-26 01:02:26 -0600

    remove old backend

--- webApp/backend/src/posts/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from posts.models import Post
-
-
-class PostsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('title', 'content')
--- webApp/backend/src/posts/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import PostListView, PostDetailView
-
-urlpatterns = [
-    path('', PostListView.as_view()),
-    path('<pk>', PostDetailView.as_view())
-]
