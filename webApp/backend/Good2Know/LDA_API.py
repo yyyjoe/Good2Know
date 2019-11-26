3443f8f2dbd9a03b27707657cda440b51bb7f380
Joe <joe@Joes-MacBook-Pro-3.local>
2019-11-26 12:17:35 -0600

    solve jsonParse

--- webApp/backend/Good2Know/LDA_API.py
+++ webApp/backend/Good2Know/LDA_API.py
@@ -9,11 +9,22 @@ from gensim import corpora, models
 import math
 import os
 import json
-
-
+import nltk
+nltk.download('wordnet')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 NUM_TOPICS=10
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
 
 class LDA(object):
     def __init__(self):
@@ -26,9 +37,8 @@ class LDA(object):
         self.stemmer = PorterStemmer()
         self.NUM_TOPICS=10
         
-        #DIC_DIR = os.path.join(BASE_DIR,"Good2Know/LDA_models/dataframe.csv")
-        DIC_DIR = "/Users/jeff/Desktop/output.csv"
-        self.db = pd.read_csv(DIC_DIR)
+        CSV_DIR = os.path.join(BASE_DIR,"Good2Know/LDA_models/output.csv")
+        self.db = pd.read_csv(CSV_DIR)
 
     # # Define stemmer and Process
     def lemmatize_stemming(self,text):
@@ -41,35 +51,41 @@ class LDA(object):
                 result.append(self.lemmatize_stemming(token))
         return result
 
+    
     def getLDA(self,text):
 
         bow = self.dictionary.doc2bow(self.preprocess(text))
 
-        data ={}
-        data["num_topics"] = self.NUM_TOPICS
-        data["topics"] = {}
-        data["posts"] = []
+        data = {
+                "num_topics": self.NUM_TOPICS,
+                "topics": {
+                    "labels":[],
+                    "posts":[],
+                },
+                "posts": [],
+            }
+
 
         nums_of_posts=np.zeros(self.NUM_TOPICS,dtype=float)
         topic_id = np.arange(self.NUM_TOPICS)
         percentage = np.zeros(self.NUM_TOPICS,dtype=float)
         for p in self.lda_model_tfidf[bow]:
-            percentage[p[0]] = p[1]
-            nums_of_posts[p[0]] = (round(p[1]*10))
+            percentage[p[0]] = str(p[1])
+            nums_of_posts[p[0]] = str(round(p[1]*10))
 
-        data["topics"]["labels"] = list(topic_id)
-        data["topics"]["data"] = list(percentage)
+
+        data["topics"]["labels"] = list((topic_id))
+        data["topics"]["data"] = list((percentage))
         for index, row in self.db.iterrows():
             #print(nums_of_posts[row['topic']])
-            if(nums_of_posts[row['topic']]>0):
-                nums_of_posts[row['topic']]-=1
-                data["posts"].append({'userID':row['ID'],
-                                      'imgURL':row['Img_URL'],
-                                      'text':row['Text'],
-                                      'date':row['Date'],
-                                      'like':row['Likes'],
+            if(nums_of_posts[row["topic"]]>0):
+                nums_of_posts[row["topic"]]-=1
+                data["posts"].append({"userID":str(row['ID']),
+                                      "imgURL":str(row['Img_URL']),
+                                      "text":str(row['Text']),
+                                      "date":str(row['Date']),
+                                      "like":str(row['Likes']),
                                      })
+        data = json.dumps((data), cls=NpEncoder)
 
-        app_json = json.dumps(str(data))
-
-        return app_json
+        return data
